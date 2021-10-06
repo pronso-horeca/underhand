28aca8d9a6d1307b0efd1e9e75654da97e828736
Artem Sorokin <sorokinjef@gmail.com>
2021-10-07 02:29:03 +0300

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,15 +20,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Underhand Speakeasy Bar`,
+        short_name: `Underhand`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#262626`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#262626`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/components/plasmic/underhand/images/logoUnderhand.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
